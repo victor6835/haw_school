34dd29ad7ad47d1a0166486dcf1062a3374893b7
victor6835 <victor6835@gmail.com>
2023-10-30 22:44:06 +0800

    10.30Js

---
+++ grandmaJavaScript/script.js
@@ -0,0 +1,42 @@
+// 基本資料型態 & 變數
+
+// 字串
+'小白'
+
+// 數字
+87
+160
+160.5
+
+// 布林值
+true
+false
+
+// 只能是英文大小寫或是數字或是$或是_的組合
+// 開頭不能是數字
+var my_name = '小黑';
+var my_age = '87';
+var is_male = 'true';
+
+document.write(my_name);
+document.write("<br>");
+my_name = '小白'
+document.write(my_name);
+document.write("<br>")
+my_name = 87;
+document.write(my_name);
+document.write("<br>");
+my_name = true
+document.write(my_name);
+
+
+document.write('有一個人叫'+my_name)
+document.write("<br>")
+document.write('小黑今年87歲')
+document.write("<br>")
+document.write(my_name+"好帥")
+document.write("<br>")
+document.write(my_name+"身高187")
+document.write("小白好帥")
+document.write("<br>")
+document.write(my_name+"討厭自己87歲")
