b51ba01b95722017411ada7d068ff4a0b461db42
victor6835 <victor6835@gmail.com>
2023-11-13 12:43:18 +0800

    1113

--- grandmaJavaScript/04.js
+++ grandmaJavaScript/04.js
@@ -2,6 +2,8 @@
 var num1 = prompt("請輸入數字一");
 var num2 = prompt("請輸入數字二");
 // parseInt: 把字串轉換成整數
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
 // parseFloat: 把字串轉換成有小數點的數
 // num1 = parseFloat(num1);
 // num2 = parseFloat(num2);
